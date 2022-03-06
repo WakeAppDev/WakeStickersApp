88effb1f2a72026b999858ec9b80fe6805e5788a
MichaelXi3 <92639450+MichaelXi3@users.noreply.github.com>
2022-03-06 18:34:05 -0500

    Detail Page 2

--- Deacon Emojis/TabViews/TabView1.swift
+++ Deacon Emojis/TabViews/TabView1.swift
@@ -6,27 +6,26 @@ struct TabView1: View {
     var columns = [GridItem(.adaptive(minimum: 160), spacing: 5)]
 
     var body: some View {
-   NavigationView {
-            ScrollView{
-                LazyVGrid(columns: columns, spacing: 15){
-                ForEach(emojisList) {Emojis in
+    NavigationView {
+        ScrollView{
+            LazyVGrid(columns: columns, spacing: 15){
+                ForEach(emojisList) { Emojis in
                     NavigationLink(destination: EmojisDetailView(emojis:Emojis)) {
                         VStack {
                             EmojisCard(emojis: Emojis)
                         }
                     }
                 }
-            }
-            .padding()
-            }
-            .navigationTitle ("Deacon's Emojis")
-            .toolbar {
-                    ToolbarItemGroup(placement: .navigationBarTrailing) {
-                        Button("Press") {
-                    }
+            } .padding()
+        }
+        .navigationTitle ("Deacon's Emojis")
+        .toolbar {
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button("Press") {
                 }
             }
         }
+    }
     }
 }
 
