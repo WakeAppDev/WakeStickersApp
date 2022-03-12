bf0150142b41fd310f538b1344d52ba075c98d61
RichardZhang0316 <zhank20@wfu.edu>
2022-03-11 23:33:14 -0500

    qwrq3rn

--- Deacon Emojis/TabViews/TabView2.swift
+++ Deacon Emojis/TabViews/TabView2.swift
@@ -3,15 +3,43 @@
 import SwiftUI
 
 struct TabView2: View {
+    init(){
+        Theme.navigationBarColors(background:newSwiftColor, titleColor: .white)
+        }
     var body: some View {
-        ZStack{
-            LinearGradient(gradient: Gradient(colors: [.red,.orange,.yellow,.green,.blue,.purple]),
-                           startPoint: .topLeading,
-                           endPoint: .bottomTrailing).edgesIgnoringSafeArea(.all)
-            
+        NavigationView{
+            VStack{
+            List{
+                Section{
+                    Button {
+                            print("button 1 tapped")
+                            } label: {
+                            Text("Privacy Policy")
+                            }
+                    Button {
+                            print("button 2 tapped")
+                            } label: {
+                            Text("Contact Us")
+                            }
+                }
+                Section{
+                    Button {
+                            print("button 3 tapped")
+                            } label: {
+                            Text("Legal Info")
+                            }
+                }
+            }
+            .navigationTitle ("About")
+            }
         }
     }
 }
+
+
+
+
+
 
 struct TabView2_Previews: PreviewProvider {
     static var previews: some View {
